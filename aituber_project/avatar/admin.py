2046671d56c6b161461a127241e9e78b2b34ecda
kyosukemiyamoto-blip <kyosuke.miyamoto@a1growth.jp>
2026-05-26 02:18:42 +0900

    Added LongTermMemory to admin.py

--- aituber_project/avatar/admin.py
+++ aituber_project/avatar/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, ShortTermMemory
+from .models import User, ShortTermMemory, LongTermMemory
 
 admin.site.register(User)
 admin.site.register(ShortTermMemory)
+admin.site.register(LongTermMemory)
